139433a6e6056732c54e2800e21358118f6ae6a9
Aadarsh Raj <aadarshraj06062001@gmail.com>
2024-03-05 02:03:29 +0530

    updated UI

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -6,26 +6,6 @@ import Projects from "./Projects";
 import Skills from "./Skills";
 import Contact from "./Contact";
 import SocialMedia from "./SocialMedia";
-
-// const Main = (prop) => {
-
-//   return (
-//     <>
-//       <main className="main" >
-//         <SocialMedia />
-//         <Header />
-       
-//         <Home />
-//         <Projects />
-//         <Skills />
-//         <Contact />
-   
-//       </main>
-//     </>
-//   );
-// };
-
-// export default Main;
 import React, { useState, useEffect } from 'react';
 import './Style/main.css'
 const Main = () => {
